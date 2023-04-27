037be0c93505d02141ca6eb34d5ab9fe6f2e258a
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-27 22:35:38 +0900

    하이라이트 생성 API 추가

--- src/main/kotlin/com/liner/api/HighlightApi.kt
+++ src/main/kotlin/com/liner/api/HighlightApi.kt
@@ -1,19 +1,37 @@
 package com.liner.api
 
+import com.liner.application.highlight.usecase.SaveHighlight
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import org.koin.core.KoinApplication
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
+import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import org.koin.ktor.plugin.koin
 
-class HighlightApi : Api({
+class HighlightApi(
+    saveHighlight: SaveHighlight
+) : Api({
     route("/highlight") {
+        post {
+            val request: SaveHighlight.Request = call.receive()
+
+            call.respond(
+                message = saveHighlight(request),
+                status = HttpStatusCode.Created
+            )
+        }
     }
 })
 
-fun Application.includeHighlight() = koin {
-    module {
-        singleOf(::HighlightApi) bind Api::class
-    }
+fun KoinApplication.includeHighlight(): Module = module {
+    singleOf(::SaveHighlight)
+    singleOf(::HighlightApi) bind Api::class
 }
---
+++ src/main/kotlin/com/liner/application/highlight/usecase/SaveHighlight.kt
@@ -0,0 +1,28 @@
+package com.liner.application.highlight.usecase
+
+import com.liner.domain.highlight.Highlight
+import com.liner.persistence.dbQuery
+import com.liner.persistence.highlight.repository.HighlightRepository
+import kotlinx.serialization.Serializable
+
+class SaveHighlight(
+    private val highlightRepository: HighlightRepository
+) {
+
+    suspend operator fun invoke(request: Request) : Response = dbQuery {
+        val highlightId: Int = highlightRepository.insert(
+            Highlight.new(content = request.content)
+        )
+
+        return@dbQuery Response(highlightId)
+    }
+
+    data class Request(
+        val content: String
+    )
+
+    @Serializable
+    data class Response(
+        val highlightId: Int,
+    )
+}
