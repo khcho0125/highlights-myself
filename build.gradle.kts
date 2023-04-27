4d9ddb5dca181e2c043dfd8fbdea799f29af759d
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-27 20:17:35 +0900

    프로젝트 세팅

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val koin_version: String by project
+val detekt_version: String by project
+val mysql_version: String by project
+val hikari_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+}
+
+group = "com.liner"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.mysql:mysql-connector-j:$mysql_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detekt_version")
+}
+
+detekt {
+    toolVersion = detekt_version
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    config.setFrom(files("$rootDir/src/main/resources/detekt-config.yml"))
+}
---
+++ src/main/kotlin/com/liner/Application.kt
@@ -0,0 +1,20 @@
+package com.liner
+
+import com.liner.config.configureDatabases
+import com.liner.config.configureHandling
+import com.liner.config.configureInject
+import com.liner.config.configureRouting
+import com.liner.config.configureSerialization
+import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>): Unit =
+    EngineMain.main(args)
+
+fun Application.module() {
+    configureInject()
+    configureHandling()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
