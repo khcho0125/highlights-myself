f5ef0b0af8d123f6a77b6a85d67ad242333f2b40
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-27 22:32:10 +0900

    유저 생성 API 추가

--- src/main/kotlin/com/liner/api/UserApi.kt
+++ src/main/kotlin/com/liner/api/UserApi.kt
@@ -5,8 +5,11 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import org.koin.core.KoinApplication
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -17,18 +20,15 @@ class UserApi(
 ) : Api({
     route("/user") {
         post {
-            val request: CreateUser.Request = call.receive()
-
-            createUser(request)
-
-            call.response.status(HttpStatusCode.Created)
+            call.respond(
+                message = createUser(),
+                status = HttpStatusCode.Created
+            )
         }
     }
 })
 
-fun Application.includeUser() = koin {
-    module {
-        singleOf(::CreateUser)
-        singleOf(::UserApi) bind Api::class
-    }
+fun KoinApplication.includeUser(): Module = module {
+    singleOf(::CreateUser)
+    singleOf(::UserApi) bind Api::class
 }
--- src/main/kotlin/com/liner/application/user/usecase/CreateUser.kt
+++ src/main/kotlin/com/liner/application/user/usecase/CreateUser.kt
@@ -1,25 +1,21 @@
 package com.liner.application.user.usecase
 
-import com.liner.config.exception.UserException
 import com.liner.domain.user.User
 import com.liner.persistence.dbQuery
 import com.liner.persistence.user.repository.UserRepository
+import kotlinx.serialization.Serializable
 
 class CreateUser(
     private val userRepository: UserRepository
 ) {
 
-    suspend operator fun invoke(request: Request): Unit = dbQuery {
-        if (userRepository.existsById(request.userId)) {
-            throw UserException.AlreadyCreated()
-        }
-
-        userRepository.insert(
-            User(id = request.userId)
-        )
+    suspend operator fun invoke(): Response = dbQuery {
+        val userId: Int = userRepository.insert(User())
+        return@dbQuery Response(userId)
     }
 
-    data class Request(
+    @Serializable
+    data class Response(
         val userId: Int
     )
 }
