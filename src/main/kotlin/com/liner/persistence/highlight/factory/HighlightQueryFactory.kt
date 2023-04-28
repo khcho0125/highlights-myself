d0deac04acfa6cdfa57ade44aa45b5fef7614936
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-28 10:30:30 +0900

    하이라이트 Repository Query 구현

--- src/main/kotlin/com/liner/persistence/highlight/factory/HighlightQueryFactory.kt
+++ src/main/kotlin/com/liner/persistence/highlight/factory/HighlightQueryFactory.kt
@@ -1,6 +1,7 @@
 package com.liner.persistence.highlight.factory
 
 import com.liner.domain.highlight.Highlight
+import com.liner.persistence.highlight.entity.HighlightStorageTable
 import com.liner.persistence.highlight.entity.HighlightTable
 import com.liner.persistence.highlight.repository.HighlightRepository
 import org.jetbrains.exposed.sql.ResultRow
@@ -10,13 +11,14 @@ import org.jetbrains.exposed.sql.select
 class HighlightQueryFactory : HighlightRepository {
     private fun toDomain(row: ResultRow) : Highlight = Highlight(
         id = row[HighlightTable.id].value,
-        content = row[HighlightTable.content],
-        storedCollectionId = null // TODO
+        content = row[HighlightTable.content]
     )
 
     override suspend fun findById(id: Int): Highlight? {
-        return HighlightTable.select { HighlightTable.id eq id }
-            .singleOrNull()?.let(::toDomain)
+        return HighlightTable
+            .select { HighlightTable.id eq id }
+            .singleOrNull()
+            ?.let(::toDomain)
     }
 
     override suspend fun insert(highlight: Highlight): Int {
