3316772383627c9950ecd153c4cf950208cff106
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-28 11:18:58 +0900

    하이라이트 애그리거트 분리

--- src/main/kotlin/com/liner/domain/highlight/Highlight.kt
+++ src/main/kotlin/com/liner/domain/highlight/Highlight.kt
@@ -2,14 +2,12 @@ package com.liner.domain.highlight
 
 data class Highlight(
     val id: Int = 0,
-    val content: String,
-    val groupList: List<Int>
+    val content: String
 ) {
 
     companion object {
         fun new(content: String): Highlight = Highlight(
-            content = content,
-            groupList = listOf()
+            content = content
         )
 
         const val CONTENT_LENGTH_LIMIT: Int = 255
---
+++ src/main/kotlin/com/liner/domain/highlight/HighlightStorage.kt
@@ -0,0 +1,6 @@
+package com.liner.domain.highlight
+
+data class HighlightStorage(
+    val highlightId: Int,
+    val collectionId: Int
+)
