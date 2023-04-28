cf81d223dc6426437e54bbe1b4219189add07ccf
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-28 12:10:36 +0900

    컬렉션 Query 추가

--- src/main/kotlin/com/liner/persistence/collection/factory/CollectionQueryFactory.kt
+++ src/main/kotlin/com/liner/persistence/collection/factory/CollectionQueryFactory.kt
@@ -1,7 +1,9 @@
 package com.liner.persistence.collection.factory
 
+import com.liner.domain.collection.Collection
 import com.liner.persistence.collection.entity.CollectionTable
 import com.liner.persistence.collection.repository.CollectionRepository
+import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.select
 
 class CollectionQueryFactory : CollectionRepository {
@@ -13,5 +15,23 @@ class CollectionQueryFactory : CollectionRepository {
                 CollectionTable.userId eq userId
             }
             .empty()
+    }
+
+    override suspend fun existsByUserIdAndNameAndParentId(userId: Int, name: String, parentId: Int?): Boolean {
+        return CollectionTable
+            .select {
+                CollectionTable.userId eq userId
+                CollectionTable.name eq name
+                CollectionTable.parentId eq parentId
+            }
+            .empty()
+    }
+
+    override suspend fun insert(collection: Collection): Int {
+        return CollectionTable.insertAndGetId {
+            it[userId] = collection.userId
+            it[name] = collection.name
+            it[parentId] = collection.parentId
+        }.value
     }
 }
--- src/main/kotlin/com/liner/persistence/collection/repository/CollectionRepository.kt
+++ src/main/kotlin/com/liner/persistence/collection/repository/CollectionRepository.kt
@@ -1,6 +1,12 @@
 package com.liner.persistence.collection.repository
 
+import com.liner.domain.collection.Collection
+
 interface CollectionRepository {
 
     suspend fun existsByIdAndUserId(id: Int, userId: Int) : Boolean
+
+    suspend fun existsByUserIdAndNameAndParentId(userId: Int, name: String, parentId: Int?) : Boolean
+
+    suspend fun insert(collection: Collection) : Int
 }
