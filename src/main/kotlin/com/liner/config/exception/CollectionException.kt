43567dd897b44ea74a219b94f77922bf9fd75667
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-30 01:13:18 +0900

    OutOfLengthName 예외 추가

--- src/main/kotlin/com/liner/config/exception/CollectionException.kt
+++ src/main/kotlin/com/liner/config/exception/CollectionException.kt
@@ -1,5 +1,7 @@
 package com.liner.config.exception
 
+import com.liner.domain.collection.Collection
+
 object CollectionException {
 
     class NotFound(override val message: String? = null) :
@@ -7,6 +9,9 @@ object CollectionException {
 
     class DuplicatedName(override val message: String? = null) :
         DomainException.Conflict(message, CollectionErrorCode.DUPLICATED_NAME)
+
+    class OutOfLengthName(override val message: String? = null) :
+        DomainException.BadRequest(message, CollectionErrorCode.OUT_OF_LENGTH_NAME)
 }
 
 enum class CollectionErrorCode(
@@ -15,7 +20,8 @@ enum class CollectionErrorCode(
 ) : ErrorCode {
 
     NOT_FOUND(1, "Collection Not Found"),
-    DUPLICATED_NAME(2, "You have a collection with the same name")
+    DUPLICATED_NAME(2, "You have a collection with the same name"),
+    OUT_OF_LENGTH_NAME(3, "Collection Name is over ${Collection.NAME_LENGTH_LIMIT} letters")
 
     ;
 
--- src/main/kotlin/com/liner/domain/collection/Collection.kt
+++ src/main/kotlin/com/liner/domain/collection/Collection.kt
@@ -1,11 +1,19 @@
 package com.liner.domain.collection
 
+import com.liner.config.exception.CollectionException
+
 data class Collection(
     val id: Int = 0,
     val userId: Int,
     val name: String,
     val parentId: Int?
 ) {
+
+    init {
+        if(name.length > NAME_LENGTH_LIMIT) {
+            throw CollectionException.OutOfLengthName()
+        }
+    }
 
     companion object {
         const val NAME_LENGTH_LIMIT: Int = 25
