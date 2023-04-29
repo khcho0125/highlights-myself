976aca131b1f285b13ec33d64632925bfd0a539b
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-29 15:44:31 +0900

    유저 ID 유효성 검사 추가

--- src/main/kotlin/com/liner/application/highlight/usecase/StoreInCollection.kt
+++ src/main/kotlin/com/liner/application/highlight/usecase/StoreInCollection.kt
@@ -2,20 +2,28 @@ package com.liner.application.highlight.usecase
 
 import com.liner.config.exception.CollectionException
 import com.liner.config.exception.HighlightException
+import com.liner.config.exception.UserException
 import com.liner.domain.highlight.HighlightStorage
 import com.liner.persistence.collection.repository.CollectionRepository
 import com.liner.persistence.dbQuery
 import com.liner.persistence.highlight.repository.HighlightRepository
 import com.liner.persistence.highlight.repository.HighlightStorageRepository
+import com.liner.persistence.user.repository.UserRepository
 import kotlinx.serialization.Serializable
 
 class StoreInCollection(
     private val collectionRepository: CollectionRepository,
     private val highlightStorageRepository: HighlightStorageRepository,
-    private val highlightRepository: HighlightRepository
+    private val highlightRepository: HighlightRepository,
+    private val userRepository: UserRepository
 ) {
 
     suspend operator fun invoke(request: Request, highlightId: Int): Unit = dbQuery {
+
+        // 유저 ID 유효성 검사
+        if(userRepository.existsById(request.userId).not()) {
+            throw UserException.NotFound()
+        }
 
         // 하이라이트 여부 검사
          if(highlightRepository.existsById(highlightId)) {
