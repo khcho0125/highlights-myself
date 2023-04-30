8436c1e8bcb9f2913c8bc8711c9f411b9543ff39
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-30 18:57:34 +0900

    컬렉션 Fixtures 추가

---
+++ src/test/kotlin/com/liner/application/collection/CollectionFixtures.kt
@@ -0,0 +1,12 @@
+package com.liner.application.collection
+
+import com.liner.domain.collection.Collection
+
+val collectionFixture: Collection by lazy {
+    Collection(
+        id = 1,
+        name = "컬렉션",
+        parentId = null,
+        userId = 1
+    )
+}
