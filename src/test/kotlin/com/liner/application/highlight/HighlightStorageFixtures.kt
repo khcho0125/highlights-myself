537d0f66ad74e7623ceab39b3df3ca0b88ec7a26
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-30 18:57:27 +0900

    하이라이트 Fixtures 추가

---
+++ src/test/kotlin/com/liner/application/highlight/HighlightStorageFixtures.kt
@@ -0,0 +1,10 @@
+package com.liner.application.highlight
+
+import com.liner.domain.highlight.HighlightStorage
+
+val highlightStorageFixtures: HighlightStorage by lazy {
+    HighlightStorage(
+        highlightId = 1,
+        collectionId = 1
+    )
+}
